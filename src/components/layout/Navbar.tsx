fb2c6c94abdfa7b9f8daa066a20c51c7bb0040c9
Mamé Azad <mazad.contractor@libertyglobal.com>
2025-02-22 12:57:15 +0100

    show  the current path on the button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,11 +8,12 @@ import {
   MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { NavLink, useLocation } from "react-router-dom";
 
 const navigation = [
-  { name: "Home", href: "home", current: true },
-  { name: "About", href: "about", current: false },
-  { name: "Projects", href: "projects", current: false },
+  { name: "Home", href: "/home", current: true },
+  { name: "About", href: "/about", current: false },
+  { name: "Projects", href: "/projects", current: false },
 ];
 
 function classNames(...classes: (string | undefined | null | false)[]): string {
@@ -20,13 +21,14 @@ function classNames(...classes: (string | undefined | null | false)[]): string {
 }
 
 const Navbar = () => {
+  const location = useLocation(); // Get current route path
   return (
     <Disclosure as="nav" className="bg-blue-400">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-700 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
@@ -44,17 +46,18 @@ const Navbar = () => {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
-                  <a
+                  <NavLink
                     key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={classNames(
-                      item.current ? "bg-slate-700 text-white" : "text-gray-300 hover:bg-slate-400 hover:text-white",
-                      "rounded-md px-3 py-2 text-sm font-medium",
-                    )}
+                    to={item.href}
+                    aria-current={location.pathname === item.href ? "page" : undefined}
+                    className={({ isActive }) =>
+                      `rounded-md px-3 py-2 text-sm font-medium ${
+                        isActive ? "bg-slate-700 text-white" : "text-gray-300 hover:bg-slate-400 hover:text-white"
+                      }`
+                    }
                   >
                     {item.name}
-                  </a>
+                  </NavLink>
                 ))}
               </div>
             </div>
@@ -119,18 +122,19 @@ const Navbar = () => {
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
           {navigation.map((item) => (
-            <DisclosureButton
+            <NavLink
               key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? "page" : undefined}
-              className={classNames(
-                item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                "block rounded-md px-3 py-2 text-base font-medium",
-              )}
+              to={item.href}
+              aria-current={location.pathname === item.href ? "page" : undefined}
+              className={({ isActive }) =>
+                classNames(
+                  isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block rounded-md px-3 py-2 text-base font-medium",
+                )
+              }
             >
               {item.name}
-            </DisclosureButton>
+            </NavLink>
           ))}
         </div>
       </DisclosurePanel>
